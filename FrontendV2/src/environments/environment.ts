5da169b1c35166b62c35ffaffb50f3205343ac9b
RickV22 <valienter69@gmail.com>
2026-07-18 22:47:03 -0500

    cloudflare

--- FrontendV2/src/environments/environment.ts
+++ FrontendV2/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://electronic-bee-dolls-letter.trycloudflare.com',
+  apiUrl: 'https://applied-chip-supervisor-result.trycloudflare.com',
   googleClientId: '248566846005-8fc3f989r904dsr8b8eghnc1b122uhm7.apps.googleusercontent.com',
 };
 
